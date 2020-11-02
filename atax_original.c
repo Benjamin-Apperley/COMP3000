ec3732c0dc8be68ea99f95fb6dd8e73f8bad6dc7
BenjaminoAcids <apperleyben@gmail.co>
2020-11-02 12:35:26 +0000

    Added main and used monotonic clock to record exectuion time

--- atax_original.c
+++ atax_original.c
@@ -1,50 +1,110 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <math.h>
-
-#include "atax.h"
-
+#include "atax_original.h"
+/* Matrix Transpose and Vector Multiplication */
 
 /* Array initialization. */
-static
-void init_array (int nx, int ny, A[][], x[])
+float A[N][N], x[N], y[N], tmp[N], test[N];
+
+int main()
+{
+	
+	struct timespec start, end;
+	uint64_t diff;
+	double gflops;
+	float out;
+
+	init_array();
+	
+	clock_gettime(CLOCK_MONTONIC, &start);
+
+	for(int i = 0; i < TIMES; i++)
+	{
+		kernel_atax();
+	}
+
+	clock_gettime(CLOCK_MONTONIC, &end);
+
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	gflops = (double) ARITHMETICSL_OPS / (diff / TIMES);
+	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+	printf("elapsed time = %llu mseconds\n", (long long unsigned int) diff/1000000);
+	printf("output = %f \n%f GigaFLOPS achieved\n", out, gflops);
+
+	return 0;
+
+}
+
+void init_array ()
 {
   int i, j;
 
-  for (i = 0; i < ny; i++)
+  for (i = 0; i < N; i++)
       x[i] = i * M_PI;
-  for (i = 0; i < nx; i++)
-    for (j = 0; j < ny; j++)
-      A[i][j] = ((DATA_TYPE) i*(j+1)) / nx;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      A[i][j] = ((DATA_TYPE) i*(j+1)) / N;
 }
 
 
 
-
-void kernel_atax(int nx, int ny,A[][], x[], y[], tmp[])
+/* Main kernels. */
+void kernel_atax()
 {
   int i, j;
 
 #pragma scop
-  	for (i = 0; i < ny; i++)
+  	for (i = 0; i < N; i++)
 	{
 		y[i] = 0;
 	}
     
-  	for (i = 0; i < nx; i++)
+  	for (i = 0; i < N; i++)
     	{
       		tmp[i] = 0;
-      		for (j = 0; j < ny; j++)
+      		for (j = 0; j < N; j++)
 		{
 			tmp[i] = tmp[i] + A[i][j] * x[j];
 		}
-      		for (j = 0; j < _PB_NY; j++)
+      		for (j = 0; j < N; j++)
 		{
 			y[j] = y[j] + A[i][j] * tmp[i];
 		}
     	}
 #pragma endscop
+
+}
+
+
+/* compare correct output */
+unsigned short int compare_atax()
+{
+
+	for (i = 0; i < N; i++)
+	{
+		test[i] = 0;
+	}
+    
+  	for (i = 0; i < N; i++)
+    	{
+      		tmp[i] = 0;
+      		for (j = 0; j < N; j++)
+		{
+			tmp[i] = tmp[i] + A[i][j] * x[j];
+		}
+      		for (j = 0; j < N; j++)
+		{
+			test[j] = test[j] + A[i][j] * tmp[i];
+		}
+    	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (equal(Y[i], test[i]) == 1)
+		{
+			return 1;
+		}
+	}
+	
+	return 0;
 
 }
 
---
+++ atax_original.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+
+#include <math.h>
+#include <stdio.h>
+
+
+
+
+#define N 10000
+#define TIMES 1000
+#define BILLION 1000000000L
+#define ARITHMETICAL_OPS N*16
+
+void init_array();
+void kernel_atax();
+
+unsigned short int compare_atax();
